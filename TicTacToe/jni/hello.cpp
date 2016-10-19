be4c2809e9596080aed13e3adfe4d541659081de
TanJain <tanmayj55@gmail.com>
2016-10-19 17:24:31 +0530

    Grid

--- TicTacToe/jni/hello.cpp
+++ TicTacToe/jni/hello.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
 
+#include <iostream>
+using namespace std;
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	char grid[7][7];
+	for(int i =0;i<7;i++){
+		for(int j =0;j<7;j++){
+			grid[i][j]='.';
+			if(j%2==0){
+				grid[i][j]='|';
+			}
+			if(i%2==0){
+				grid[i][j]='-';
+			}
+		}
+	}
+	for(int k=0;k<7;k++){
+		for(int l=0;l<7;l++){
+			cout<<grid[k][l];
+		}
+		cout<<endl;
+	}
+	
 }
