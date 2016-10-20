95930c0d30f6593121e26912aed75c43aa61bfca
TanJain <tanmayj55@gmail.com>
2016-10-20 15:09:38 +0530

    Complete

--- TicTacToe/jni/hello.cpp
+++ TicTacToe/jni/hello.cpp
@@ -3,8 +3,13 @@
 using namespace std;
 int main()
 {
+	//Declarations
+	int win =0; // 1 if anybody wins else 0
+	int t =0; // Determines whose turn it is player if 0 , else player 2
+	char turn[2]; //Stores Symbol used by user to play the game
+	char grid[7][7];// Game grid
+	int coord[2]; // Stores Move coordinates
 	//Creating Game grid
-	char grid[7][7];
 	for(int i =0;i<7;i++){
 		for(int j =0;j<7;j++){
 			grid[i][j]=' ';
@@ -17,23 +22,26 @@ int main()
 		}
 	}
 	//Choosing Symbols
-	int t=0;
-	char turn[2];
 	cout<<"Enter First player's symbol\n";
 	cin>>turn[0];
 	cout<<"Enter Second player's symbol\n";
 	cin>>turn[1];
-	// move coordinate
-	int coord[2];
 	//Game loop
 	while(1){
+		//Checking if anyone won
+		if(win){
+			cout<<"Player "<<(!t)+1<<" Wins\n";
+			break;
+		}
+		//Priting game
 		for(int k=0;k<7;k++){
 			for(int l=0;l<7;l++){
 				cout<<grid[k][l];
 			}
 			cout<<endl;
 		}
-		cout<<"Player "<<t+1<<"'s Move\n";
+		cout<<"\nPlayer "<<t+1<<"'s Move\n";
+		// Taking move coordinates
 		while(1){
 			cin>>coord[0];
 			cin>>coord[1];
@@ -46,6 +54,25 @@ int main()
 			}
 			
 		}
+		// Checking rows & columns
+		for(int c=1;c<6;c+=2){
+				if((grid[c][1]==grid[c][3]&&grid[c][3]==grid[c][5])&&grid[c][1]!=' '){
+					win++;
+				}
+				if((grid[1][c]==grid[3][c]&&grid[3][c]==grid[5][c])&&grid[1][c]!=' '){
+					win++;
+				}
+		}
+		//Checking Diagnols
+		if(grid[3][3]!=' '){
+			if(grid[3][3]==grid[1][1]&&grid[1][1]==grid[5][5]){
+				win++;
+			}
+			else if(grid[3][3]==grid[1][5]&&grid[1][5]==grid[5][1]){
+				win++;
+			}
+		}
+		// Next turn Changing player number
 		t=!t;
 	}
 	
